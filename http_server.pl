69c42fbc74211d0cedcd2652c390f2c9d5812b11
Aditya Chinchure <aditya.chinchure@gmail.com>
2019-11-28 14:23:02 -0800

    Updated http server

--- http_server.pl
+++ http_server.pl
@@ -1,11 +1,48 @@
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_error)).
+:- use_module(library(http/html_write)).
+:- use_module(library(http/http_parameters)).
 
-:- http_handler(root(hello_world), say_hi, []).		
+:- http_handler('/query', handle_query, []).
 
-server(Port) :-						
-        http_server(http_dispatch, [port(Port)]).
+server(Port) :-
+	http_server(http_dispatch, [port(Port)]).
 
-say_hi(_Request) :-					
-        format('Content-type: text/plain~n~n'),
-        format('Hello World!~n').
+	/*  
+	    browse http://127.0.0.1:8000/?baz=3&baz=4
+	    This demonstrates handling parameters.
+	    Not all type checking options are demonstrated, see
+	    http://www.swi-prolog.org/pldoc/doc_for?object=section%283,%273.9%27,swi%28%27/doc/packages/http.html%27%29%29
+	 */
+
+	handle_query(Request) :-
+		reply_html_page(
+				title('Howdy'),
+				[\page_content(Request)]).
+
+		page_content(Request) --> 
+{
+	% catch because http_parameters throws if a param is invalid
+		catch(
+				http_parameters(Request,
+					[   
+					% default for a missing param
+					q(Q, [default('this is default query')])]),
+				_E, 
+				fail),
+	!   
+},
+	html(
+			[   
+			h1('Some Page'),
+			p('The body goes here'),
+			p('The query is ~w'-Q)
+			]). 
+
+	page_content(_Request) --> 
+	html(
+			[   
+			h1('Oops!'),
+			p('Some parameter wasnt valid')
+			]). 
